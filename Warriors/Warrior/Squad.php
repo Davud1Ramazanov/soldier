b93ec6a2685b8f47c8e5bd460ea32e62fa5547db
Davud1Ramazanov <Rama_bz54@student.itstep.org>
2023-04-18 01:23:30 +0400

    A squad with fighters has been added

---
+++ Warriors/Warrior/Squad.php
@@ -0,0 +1,52 @@
+<?php
+
+class Squad
+{
+    private $name;
+    private $warrios;
+    private $min_warrios;
+    private $max_warrios;
+
+    public function __construct($name, $commander)
+    {
+        $this->name = $name;
+        $this->min_warrios = 5;
+        $this->max_warrios = 25;
+        $this->warrios = array();
+        array_push($this->warrios, $commander);
+    }
+
+    public function setMinWarrios($min_warrios)
+    {
+        $this->min_warrios = $min_warrios;
+    }
+
+    public function setMaxWarrios($max_warrios)
+    {
+        $this->max_warrios = $max_warrios;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function delWarrios($warrior)
+    {
+        array_shift($this->warrios, $warrior);
+    }
+
+    public function addWarrios($warrior)
+    {
+        if ($this->warrios < $this->max_warrios) {
+            array_push($this->warrios, $warrior);
+        }
+    }
+}
+
+?>
