0427e4ea8945a0de26d178e55cdf435f4573d5b5
Davud1Ramazanov <Rama_bz54@student.itstep.org>
2023-04-18 21:25:00 +0400

    Few changes

--- Warriors/Warrior/Commander.php
+++ Warriors/Warrior/Commander.php
@@ -1,16 +1,26 @@
 <?php
-abstract class Commander extends Warrior{
-    private $rank;
-    private $specialization;
-    private $reputation;
-    private $achivments_command;
 
-    public function __construct($rank, $specialization, $reputation)
+abstract class Commander extends Warrior
+{
+    private $name;
+    private $rank;
+    private $reputation;
+    private $weapon;
+    private $endurance;
+    private $protection;
+    private $speed;
+
+
+    public function __construct($name, $weapon, $endurance, $protection, $speed)
     {
-        $this->rank = $rank;
-        $this->specialization = $specialization;
-        $this->reputation = $reputation;
-        $this->achivments_command = new AchivmentsCommander();
+        $this->name = $name;
+        $this->rank = 1;
+        $this->reputation = 10;
+        $this->achivments_command = array(new AchivmentsWarrior());
+        $this->weapon = $weapon;
+        $this->endurance = $endurance;
+        $this->protection = $protection;
+        $this->speed = $speed;
     }
 
     public function setRank($rank)
@@ -18,19 +28,24 @@ abstract class Commander extends Warrior{
         $this->rank = $rank;
     }
 
-    public function setSpecialization($specialization)
-    {
-        $this->specialization = $specialization;
-    }
-
-    public function setReputation($reputation)
-    {
-        $this->reputation = $reputation;
-    }
-
     public function getSpecialization()
     {
         return $this->specialization;
+    }
+
+    public function setEndurance($endurance)
+    {
+        $this->endurance = $endurance;
+    }
+
+    public function setProtection($protection)
+    {
+        $this->protection = $protection;
+    }
+
+    public function setSpeed($speed)
+    {
+        $this->speed = $speed;
     }
 
     public function getRank()
@@ -42,5 +57,21 @@ abstract class Commander extends Warrior{
     {
         return $this->reputation;
     }
+
+    public function getEndurance()
+    {
+        return $this->endurance;
+    }
+
+    public function getProtection()
+    {
+        return $this->protection;
+    }
+
+    public function getSpeed()
+    {
+        return $this->speed;
+    }
 }
+
 ?>
